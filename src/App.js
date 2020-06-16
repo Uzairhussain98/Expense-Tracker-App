1ec70c3a7ba4dfbbde1ee55ec4cd7fdbaef027ed
Uzair Hussain <uzairhussain98@hotmail.com>
2020-06-17 02:58:59 +0500

    implemented UI only adding funcationality left

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Balance from './components/Balance';
+import Accountsummary from './components/Accountsummary';
+import Transactionlist from './components/Transactionlist';
+import Addtransaction from './components/Addtransaction';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
+      <Header/>
+      <div className="container">
+        <Balance />
+        <Accountsummary/>
+        <Transactionlist/>
+        <Addtransaction/>
+        </div>
+
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    
+      <h2>Expense Tracker By Uzair Hussain </h2>
+    
+  )
+}
+
+export default Header;
