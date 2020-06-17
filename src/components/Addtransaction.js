55bad85d847aeb9a2c9a91826ae31cd028f65457
Uzair Hussain <uzairhussain98@hotmail.com>
2020-06-17 21:37:29 +0500

    app completed

--- src/components/Addtransaction.js
+++ src/components/Addtransaction.js
@@ -1,4 +1,6 @@
-import React ,{ useState } from "react"
+import React ,{ useState , useContext} from "react"
+import {GlobalContext} from '../contexts/Globalstate'
+
  
 
 const Addtransaction = () => {
@@ -6,11 +8,23 @@ const Addtransaction = () => {
     const [text,settext] = useState('');
     const [amount,setamount] = useState(0);
 
+    const {  addTransaction } = useContext(GlobalContext);
+
+    const onSubmit = e => {
+      e.preventDefault();
+
+      const newTransaction = {
+        id : Math.floor(Math.random() * 100000000),
+        text,
+        amount: +amount
+      }
+        addTransaction(newTransaction);
+    }
 
   return (
     <>
         <h3>Add new transaction</h3>
-      <form >
+      <form onSubmit={onSubmit} >
         <div className="form-control">
           <label htmlFor="text">Text</label>
           <input type="text" value={text} onChange={(e) => settext(e.target.value)}  placeholder="Enter text..." />
